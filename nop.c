75c235808b4460e2436858d8f38ac0d0243f59ec
Sonnydread <alvaradoguille590@gmail.com>
2023-05-13 14:01:37 -0700

    monty

--- nop.c
+++ nop.c
@@ -5,12 +5,6 @@
  * @line_number: current line
  */
 
-/**
- * #\define __attribute__((unused)) UNUSED
- * Does not work with the given flags or the compiler
- * tested without the '\' escaping character
- */
-
 void nop_op(__attribute__((unused))stack_t **stack,
 __attribute__((unused))unsigned int line_number)
 {
