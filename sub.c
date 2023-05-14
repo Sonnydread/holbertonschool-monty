845d6196cb9092d624fe931d26b1c320408d51b4
Sonnydread <alvaradoguille590@gmail.com>
2023-05-14 07:56:02 -0700

    f_sub

--- sub.c
+++ sub.c
@@ -15,7 +15,7 @@ for (nodes = 0; aux != NULL; nodes++)
 aux = aux->next;
 if (nodes < 2)
 {
-fprintf(stderr, "L%d: can´t sub, stack too short\n", counter);
+fprintf(stderr, "L%d: can't sub, stack too short\n", counter);
 freeStack();
 fclose(var.fp);
 exit(EXIT_FAILURE);
