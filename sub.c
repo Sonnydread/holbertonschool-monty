ff3c0913c04b47389ecd41c1b00a3a15cab5f952
Sonnydread <alvaradoguille590@gmail.com>
2023-05-13 20:25:40 -0700

    monty

--- sub.c
+++ sub.c
@@ -8,19 +8,20 @@
 void f_sub(stack_t **head, unsigned int counter)
 {
 stack_t *aux;
-int subs, nodes;
+int sus, nodes;
 
 aux = *head;
 for (nodes = 0; aux != NULL; nodes++)
 aux = aux->next;
 if (nodes < 2)
 {
-fprintf(stderr, "L%d: can´t subs, stack too short\n", counter);
+fprintf(stderr, "L%d: can´t sub, stack too short\n", counter);
+freeStack();
 exit(EXIT_FAILURE);
 }
 aux = *head;
-subs = aux->next->n - aux->n;
-aux->next->n = subs;
+sus = aux->next->n - aux->n;
+aux->next->n = sus;
 *head = aux->next;
 free(aux);
 }
